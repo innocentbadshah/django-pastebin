014e3e1484e41eb52dc20f430f1ee96a68f74517
Ashish Singh <ak1186680@gmail.com>
2018-03-28 21:45:39 +0530

    Register to admin

--- snippets/models.py
+++ snippets/models.py
@@ -3,11 +3,20 @@ from django.db import models
 # Create your models here
 
 class Snippet(models.Model):
+    
+    def __str__(self):
+        return self.title
+    
     title = models.CharField(max_length = 30)
     content = models.TextField()
     datetime_created = models.DateTimeField(auto_now = True)
 
+
 class Comment(models.Model):
+    
+    def __str__(self):
+        return "{0}: {1}".format(str(self.snippet), self.content)
+    
     snippet = models.ForeignKey(to = 'Snippet', on_delete = models.CASCADE)
     content = models.TextField()
     datetime_created = models.DateTimeField(auto_now = True)
